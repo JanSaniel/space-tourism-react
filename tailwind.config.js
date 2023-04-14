5d56a257317eb54f39390786a9cda9ee1ce3f8cf
JanSaniel <jsaniel@deloitte.com>
2023-04-14 19:38:25 +0800

    Creating Navbar and Hero Component

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {},
+    fontFamily: {
+      bellefair: ["Bellefair","serif"],
+      barlowCondensed: ["Barlow Condensed","sans-serif"],
+      barlow: ["Barlow","sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
