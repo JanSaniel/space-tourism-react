306135d40076f89ca70dac2ef3a98c6a0b8ef176
JanSaniel <jsaniel@deloitte.com>
2023-04-18 19:39:03 +0800

    homepage update with circle button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className='text-white'>
-      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-left flex flex-col justify-center'>
+      
+      <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-left flex flex-col justify-center relative'>
         <p className='md:text-[28px] text-[#D0D6F9] font-barlowCondensed  p-1'>
             SO, YOU WANT TO TRAVEL TO
         </p>
@@ -11,10 +12,9 @@ const Hero = () => {
           SPACE
         </h1>
         <p className='max-w-[444px] md:text-[18px] text-xl  font-barlow text-[#D0D6F9]'>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
-        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="21"><g fill="#D0D6F9" fill-rule="evenodd"><path d="M0 0h24v3H0zM0 9h24v3H0zM0 18h24v3H0z"/></g></svg>
-  <span class="sr-only">Icon description</span>
-</button>
+        <button class=" absolute w-72 h-72 rounded-full bg-white hover:bg-red-500 text-black right-0">
+            Explore
+        </button>
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
+
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4  text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48"><g fill="none" fill-rule="evenodd"><circle cx="24" cy="24" r="24" fill="#FFF"/><path fill="#0B0D17" d="M24 0c0 16-8 24-24 24 15.718.114 23.718 8.114 24 24 0-16 8-24 24-24-16 0-24-8-24-24z"/></g></svg>
+        </div>
         <ul className='flex'>
           <li className='flex-none p-4'>00 Home</li>
           <li className='flex-none p-4'>01 Destination </li>
